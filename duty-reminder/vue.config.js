8173d1742cd432c291c59770ade4a169af5655fc
JC <56730722+ScoobyLuffyDoo@users.noreply.github.com>
2023-08-04 16:31:31 +0200

    Second change

--- duty-reminder/vue.config.js
+++ duty-reminder/vue.config.js
@@ -1,8 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {
-    entry: './duty-reminder/src/main.js'
-  }
+  transpileDependencies: true
 })
