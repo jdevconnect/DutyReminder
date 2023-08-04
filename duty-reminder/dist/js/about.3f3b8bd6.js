ba967b1f02fcb5c07322a4050c34198a38f356ae
JC <56730722+ScoobyLuffyDoo@users.noreply.github.com>
2023-08-04 12:51:27 +0200

    dist

---
+++ duty-reminder/dist/js/about.3f3b8bd6.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkduty_reminder"]=self["webpackChunkduty_reminder"]||[]).push([[443],{833:function(n,e,r){r.r(e),r.d(e,{default:function(){return f}});var u=r(3396);const t={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),s=[a];function i(n,e){return(0,u.wg)(),(0,u.iD)("div",t,s)}var c=r(89);const d={},o=(0,c.Z)(d,[["render",i]]);var f=o}}]);
+//# sourceMappingURL=about.3f3b8bd6.js.map
